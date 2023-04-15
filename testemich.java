942dfd4cb69a03cb48a7e61897779d84f02fd3c9
Ugur1812 <ugurayvaz1812@gmail.com>
2023-04-16 00:33:16 +0200

    created Java Klasse

---
+++ testemich.java
@@ -0,0 +1,13 @@
+public class testemich {
+
+    public static void main(String[] args) {
+        System.out.println("hey");
+        int i =getMethod(2);
+        System.out.println(i);
+    }
+
+    public static int getMethod( int i){
+        return 4;
+    }
+
+}
